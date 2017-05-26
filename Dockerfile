92ff4404d1780322b656ab2df129eddb65a20d72
Daniel Holzmann <d@velopment.at>
2017-05-26 12:20:01 +0200

    fix it

--- Dockerfile
+++ Dockerfile
@@ -4,8 +4,4 @@ MAINTAINER Daniel Holzmann <d@velopment.at>
 
 COPY tools /opt/tools
 
-ENV USERNAME dev
-
 RUN /opt/tools/install.sh
-
-USER $USERNAME
--- tools/install.sh
+++ tools/install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-/opt/tools/install-meteor.sh
-/opt/tools/install-yarn.sh
+# we're already running as user "dev" here, so we have to use sudo
+sudo /opt/tools/install-meteor.sh
+sudo /opt/tools/install-yarn.sh
 
 # clean up
-apt-get clean
-rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+sudo apt-get clean
+sudo rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
